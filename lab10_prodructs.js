d64428d56e723631da22efa160c652b97effc64f
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-01-08 07:32:57 -0500

    local storage exercise

---
+++ lab10_prodructs.js
@@ -0,0 +1,6 @@
+﻿//LAB 10-DATA STORAGE: PRODUCTS PAGE
+//alert("products");//DELETE AFTER CONFIRMING CONNECTION
+
+
+document.body.style.background = localStorage.getItem("inColor");
+document.getElementById("newMsgBox").innerHTML += " " + localStorage.getItem("username");
